b0c5ee6e2ea25330119d5ec0899cb1a9b12df105
Patrick Mediodia <mediodia.patrick@gmail.com>
2022-12-22 17:01:13 +0800

    Fixed merge conflict

--- API_requests.py
+++ API_requests.py
@@ -12,9 +12,7 @@ def authenticate(username, password):
     
     end_point = url + 'auth/local'
 
-    headers = {
-      'Content-Type': 'application/json'
-    }
+    headers = { 'Content-Type': 'application/json' }
     
     payload = json.dumps({
       "identifier": username,
@@ -26,9 +24,7 @@ def authenticate(username, password):
     try:
         account_dict = json.loads(response.text)
         account_object = Account(**account_dict)
-        
-        jwt = account_object.jwt
-        
+        jwt = account_object.jwt    
         return account_object
     except:
         return None
@@ -48,55 +44,11 @@ def get_user_data(jwt, trigger_word):
     end_point += '&populate[3]=courses.modules.lessons.questions'
     end_point += '&fields=id,username,email'
     
-    headers = {
-        'Authorization': 'Bearer ' + jwt
-    }
+    headers = { 'Authorization': 'Bearer ' + jwt }
     
     response = requests.request("GET", end_point, headers=headers)
 
     user_data_dict = json.loads(response.text)
     user_data_object = UserData(**user_data_dict)
-<<<<<<< HEAD
-   
+    
     return user_data_object
-=======
-
-    for course in user_data_object.courses:
-        for module in course.modules:
-            for lesson in module.lessons:
-                if lesson.trigger_word is None:
-                    continue
-                if trigger_word in lesson.trigger_word:
-                    print('Found trigger word')
-                    return lesson
-                
-    return None
-
-def detect_intent(project_id, session_id, text, language_code):    
-    session_client = dialogflow.SessionsClient()
-    
-    session = session_client.session_path(project_id, session_id)
-    print("Session path: {}\n".format(session))
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
-    query_input = dialogflow.QueryInput(text=text_input)
-    
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-    
-    '''
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
-        )
-    )
-    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
-    '''
-    
-    return response
->>>>>>> 0a6533be83333efc7d1f7ab497cb284fe89061fc
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import threading
 from ALSA_handler import noalsaerr
-from text_to_speech import gtts_speak
 from API_requests import authenticate
+from text_to_speech import gtts_speak
 from speech_to_text import speech_to_text
 from whizzy_avatar import initialize_avatar
 from web_searching import start_google_assistant
 from picovoice.detect_hotword import detect_hotword
-<<<<<<< HEAD
-=======
-from API_requests import authenticate, detect_intent
-from google.protobuf.json_format import MessageToDict
->>>>>>> 0a6533be83333efc7d1f7ab497cb284fe89061fc
 from interactive_discussion import start_interactive_discussion
 from smart_controls import initialize_devices, start_smart_controls
 
@@ -30,7 +25,10 @@ mode_map = {
 username = "faculty1"
 password = "123456"
 
-<<<<<<< HEAD
+#Session id must be unique to each raspberry PI
+#Denotes the current conversation
+session_id = 'RoomNumber'
+
 current_mode = modes[2]
 
 def change_mode(current_mode, command):
@@ -39,54 +37,7 @@ def change_mode(current_mode, command):
             if mode in command and current_mode != mode:
                 return mode
     return current_mode
-=======
-#Session id must be unique to each raspberry PI
-#Denotes the current conversation
-session_id = 'RoomNumber'
-project_id = 'whizzy-1d843'
-response = None
 
-current_mode = modes[0]
-
-def process_input():
-    response_json = None
-    hasResponse = False
-    
-    while True:
-        command = speech_to_text()
-        
-        #get intent of comand
-        response_object = detect_intent(project_id, session_id, command, 'en')
-        response_json = MessageToDict(response_object._pb)
-        
-        print(response_json['responseId'])
-        
-        #get another respose
-        for text_object in response_json['queryResult']['fulfillmentMessages']:
-            for text in text_object['text']['text']:
-                if text != '':
-                    gtts_speak(text)
-                else:
-                    break
-                
-    return response_json
->>>>>>> 0a6533be83333efc7d1f7ab497cb284fe89061fc
-
-def change_mode(requested_mode):
-    global current_mode
-    
-    if requested_mode == current_mode:
-        gtts_speak(f'Already in {current_mode}')
-        return
-    
-    for mode in modes:
-        if requested_mode.lower() == mode and current_mode != mode:
-            current_mode = mode
-            gtts_speak(f'Switched to {mode}')
-            break
-    else:
-        gtts_speak('Mode not found')
-        
 def main():
     global current_mode
     account = authenticate(username, password)
@@ -108,21 +59,25 @@ def main():
         while True:
             print(f'Current mode: {current_mode}')
             if detect_hotword():
-                response = process_input()
-                if response is not None:
-                    action = response['queryResult']['action']
-                    
-                    #change mode
-                    if  action == 'ChangeMode':
-                        change_mode(response['queryResult']['parameters']['Mode'])
-                        
-                    #current mode of Whizzy
-                    elif action == 'CurrentMode':
-                        gtts_speak(f"Currently I am in the {current_mode} mode")
-                        
-                    #send command to current mode
-                    else:
-                        mode_map[current_mode](command)
+                command = speech_to_text()
+                
+                #command is empty, ignore
+                if command == '':
+                    continue
+                
+                #change modes
+                new_mode = change_mode(current_mode, command)
+                if new_mode != current_mode:
+                    current_mode = new_mode
+                    gtts_speak(f'Switched to {new_mode}')
+                
+                #to get the current mode of Whizzy
+                elif "current" and "mode" in command:
+                    gtts_speak(f"Currently I am in the {current_mode} mode")
+                
+                #send command to current mode
+                else:
+                    mode_map[current_mode](command)
                     
 if __name__ == '__main__':
     main()
