a9b10a61807398e75dcaa6c1668042361d1b91d2
patrickmediodia <mediodia.patrick@gmail.com>
2022-12-23 21:37:56 +0800

    Added API request to get status of devices

--- RaspberryPi/API_requests.py
+++ RaspberryPi/API_requests.py
@@ -2,7 +2,6 @@ import json
 import requests
 from models.account import Account
 from models.userData import UserData
-from google.cloud import dialogflow
 
 url = "https://api.jhonlloydclarion.online/api/"
 jwt = None
@@ -34,7 +33,7 @@ def get_jwt_token():
         print('No jwt token, please authenticate')
         return
     return jwt
-
+    
 def get_user_data(jwt, trigger_word):
     end_point = url + 'users/me?'
     end_point += 'populate[0]=courses'
@@ -52,3 +51,47 @@ def get_user_data(jwt, trigger_word):
     user_data_object = UserData(**user_data_dict)
     
     return user_data_object
+        
+class Room:
+    def __init__(self, id, attributes):
+        self.id = id
+        self.attributes = RoomAttributes(**attributes)
+    	
+class RoomAttributes:
+    def __init__(self, createdAt, updatedAt, publishedAt, name, devices):
+        self.createdAt = createdAt,
+        self.updatedAt = updatedAt,
+        self.publishedAt = publishedAt,
+        self.name = name
+        self.devices = [ Device(**device) for device in devices['data'] ]
+    
+class Device:
+    def __init__(self, id, attributes):
+        self.id = id
+        self.attributes = DeviceAttributes(**attributes)
+
+class DeviceAttributes:
+    def __init__(self, name, identifier, status, createdAt, updatedAt, publishedAt):
+        self.name = name
+        self.identifier = identifier
+        self.status = status
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.publishedAt = publishedAt
+	
+def get_room_device_data(jwt, room_number):
+    end_point = url + 'rooms?'
+    end_point += 'populate=devices'
+    end_point += f'&filters[name][$eq]={room_number}'
+    
+    headers = { 'Authorization': 'Bearer ' + jwt }
+    
+    response = requests.request("GET", end_point, headers=headers)
+    
+    print(response.text)
+
+    room_device_data = json.loads(response.text)
+
+    print(room_device_data)
+
+get_room_device_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjcxNzc1MTk4LCJleHAiOjE2NzQzNjcxOTh9.HPLFNXzrAJXi8Zcg9Kyepg5Fl9xk0SPQQeuroNZF-_c', 'R200')    
