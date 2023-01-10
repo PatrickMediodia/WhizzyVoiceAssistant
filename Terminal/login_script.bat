d2e438b00f2ac3b7b7c75227637a4d49c69da1f9
unknown <mediodia.patrick@gmail.com>
2023-01-10 19:13:25 +0800

    Updated windows login script and added instructions

--- Terminal/login_script.bat
+++ Terminal/login_script.bat
@@ -1,6 +1,23 @@
-:: run commands below on terminal
-:: winrm qc
-:: winrm set winrm/config/service @{AllowUnencrypted="true"}
+:: Useful Links
+:: Documentation : https://learn.microsoft.com/en-us/windows/win32/winrm/installation-and-configuration-for-windows-remote-management
+:: Guide : https://stackoverflow.com/questions/32324023/how-to-connect-to-remote-machine-via-winrm-in-python-pywinrm-using-domain-acco
+:: Pywinrm Documentation : https://github.com/diyan/pywinrm
+
+:: Execute the following commands below (admin)
+:: cmd
+::      winrm qc / winrm quickconfig
+::      winrm set winrm/config/service @{AllowUnencrypted="false"}
+::      winrm set winrm/config/service/auth @{Basic="false"}
+::      winrm set winrm/config/client/auth @{Basic="false"}
+:: powershell
+::      Invoke-Expression ((New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))
+
+:: Change settings in services
+:: 1. windows + r
+:: 2. services.msc
+:: 3. locate "Windows Remote Management" service
+:: 4. go to properties and set startup type from "Automatic (Delayed Start) to Automatic"
+:: 5. click apply then ok
 
 @echo off
 setlocal enabledelayedexpansion
