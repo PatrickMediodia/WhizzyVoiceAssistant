f6551f7e283ee795b46c3256364485e4a316a377
Patrick Mediodia <mediodia.patrick@gmail.com>
2023-01-27 01:06:32 +0800

    Initial mic indication graphic

--- RaspberryPi/speech_to_text.py
+++ RaspberryPi/speech_to_text.py
@@ -1,6 +1,7 @@
 import os
 import threading
 import speech_recognition as sr
+from whizzy_avatar import set_mic_state
 from text_to_speech import gtts_speak, get_response
 
 def speech_to_text():
@@ -12,9 +13,13 @@ def speech_to_text():
         print('Listening to command ...')
         os.system("mpg123 audio/ding_sound.mp3 >/dev/null 2>&1")
         
+        set_mic_state(True)
+        
         #automatically sets the energy threshold
         listener.adjust_for_ambient_noise(source, duration=0.5)
         voice_data = listener.listen(source)
+        
+        set_mic_state(False)
         
         try:
             command = listener.recognize_google(voice_data)
--- RaspberryPi/whizzy_avatar.py
+++ RaspberryPi/whizzy_avatar.py
@@ -6,6 +6,11 @@ import time
 talking = False
 screen = None
 handler = None
+mic_flag = False
+small = False
+
+white = (255, 255, 255)
+yellow = (255, 255, 0)
 
 mode_text = ''
 lesson_text = 'Lesson: Please load a lesson'
@@ -37,11 +42,6 @@ class imageHandler:
         else:
             surface.blit(pygame.transform.smoothscale(self.pics[id], size), (picX, picY))
             
-#pass new avatar talking state
-def change_avatar_state(avatar_state):
-    global talking
-    talking = avatar_state
-
 #images
 def display():
     #not talking
@@ -70,48 +70,79 @@ def face():
             handler.render(screen, "1", (A, B), True, (x, y))
 
             display_text()
+            
             time.sleep(0.3)
 
         elif talking == True:
-
             if count <= 2:
                 count += 1
             img = str(count)
+            
             handler.render(screen, img, (A, B), True, (x, y))
 
             display_text()
-
+            
             time.sleep(0.3)
             count += 1
             if count == 8:
                 count = 2
-
+        
         pygame.display.update(A, B, x, y)
         
 # displays text
 def display_text():
-    white = (255, 255, 255)
     width, height = 400, 70
     
     #display current mode
-    x, y = 100, 90
     font = pygame.font.Font('avatar/comicsans.TTF', 35, bold= True)
     mode_text_render = font.render(mode_text, True, white)
     text_width = mode_text_render.get_width()
+    
+    x, y = 100, 90
     pygame.draw.rect(screen, white, (x, y, text_width+50, height+10), 3, 10)  # rectangle
     screen.blit(mode_text_render, (x+23, y+12, width, height))  # text
     
     if 'Interactive Discussion' in mode_text:
+        #display current lesson
         lesson_text_render = font.render(lesson_text, True, white)
         text_width = lesson_text_render.get_width()
         
-        #display current lesson
         x_2,y_2 = 1920-(text_width+150), 90
-        
         pygame.draw.rect(screen, white, (x_2, y_2, text_width+50, height+10), 3, 10)  # rectangle
         screen.blit(lesson_text_render, (x_2+23, y_2+12, width, height))  # text
         
+    mic()
     pygame.display.flip()
+    
+def mic():
+    global small
+    coordinate = (150, 1080-125)
+    
+    #display circle
+    if mic_flag is True and small is True:
+        pygame.draw.circle(screen, yellow, coordinate, 80, 100)
+        small = False
+        
+    elif mic_flag is True and small is False:
+        pygame.draw.circle(screen, yellow, coordinate, 90, 100)
+        small = True
+        
+    else:
+        pygame.draw.circle(screen, white, coordinate, 80, 100)
+    
+    #display image
+    mic_img = pygame.image.load('avatar/mic.bmp').convert_alpha()
+    mic_img = pygame.transform.scale(mic_img, (64, 109.5))
+    screen.blit(mic_img, (120, 902))
+
+def set_mic_state(mic_state):
+    global mic_flag
+    mic_flag = mic_state
+
+#pass new avatar talking state
+def change_avatar_state(avatar_state):
+    global talking
+    talking = avatar_state
 
 def set_mode_text(text):
     global mode_text
