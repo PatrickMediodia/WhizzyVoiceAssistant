b8a9535674bc83a148900bae3286cb145ebbef02
Faculty1 <mediodia.patrick@gmail.com>
2023-02-24 15:34:48 +0800

    Added checking if application is already open

--- Terminal/server.py
+++ Terminal/server.py
@@ -40,13 +40,19 @@ def server():
                     
                 if 'open' in command:
                     if 'blackboard learn' in command:
-                        threading.Thread(target=open_blackboard, daemon=True, args=[token, user_id, application_instance]).start()
-                        message = 'Blackboard Learn has been opened'
+                        if application_instance['blackboard'] is None:
+                            threading.Thread(target=open_blackboard, daemon=True, args=[token, user_id, application_instance]).start()
+                            message = 'Blackboard Learn has been opened'
+                        else:
+                            message = 'Blackboard Learn is already open'
 
                     elif 'microsoft teams' in command:
-                        threading.Thread(target=open_teams, daemon=True, args=[token, user_id]).start()
-                        application_instance['teams'] = 'Open'
-                        message = 'Microsoft Teams has been opened'
+                        if application_instance['teams'] is None:
+                            threading.Thread(target=open_teams, daemon=True, args=[token, user_id]).start()
+                            application_instance['teams'] = 'teams'
+                            message = 'Microsoft Teams has been opened'
+                        else:
+                            message = 'Microsoft Teams is already open'
 
                 elif 'close' in command:
                     if 'blackboard learn' in command:
@@ -54,6 +60,8 @@ def server():
                             message = 'Blackboard Learn is not open'
                         else:
                             application_instance['blackboard'].close()
+                            application_instance['blackboard'] = None
+
                             print('\nClosing blackboard .....\n')
                             message = 'Blackboard Learn has been closed'
 
@@ -62,6 +70,7 @@ def server():
                             message = 'Microsoft Teams is not open'
                         else:
                             close_teams()
+                            
                             application_instance['teams'] = None
                             message = 'Microsoft Teams has been closed'
 
