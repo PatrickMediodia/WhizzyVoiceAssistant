684d4bd632b8d05859cdbc3df22af66255b5f819
Patrick Mediodia <mediodia.patrick@gmail.com>
2023-02-25 15:27:57 +0800

    Added alternatives to gTTs

--- RaspberryPi/text_to_speech.py
+++ RaspberryPi/text_to_speech.py
@@ -14,7 +14,8 @@ def get_response(type):
         return random.choice(responses[type])
     except: 
         return 'Dialog not found'
-    
+
+#google text to speech (online)
 def gtts_speak(audio_string):
     try:
         tts = gTTS(text=audio_string, lang='en', tld='com')
@@ -29,15 +30,32 @@ def gtts_speak(audio_string):
         print(e)
         
 '''
+#festival, sound ok offline but is slow
+def gtts_speak(audio_string):
+    os.system(f'echo "{audio_string}" | festival --tts')
+'''
+
+'''
+#PYTTSX3, sounds bad offline but is fast
 import pyttsx3
 
-engine = pyttsx3.init('espeak')
-engine.setProperty('rate', 150)
+engine = pyttsx3.init()
+engine.setProperty('rate', 125)
 
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[12])
+print(voices)
+engine.setProperty('voice', voices[12].id)
 
 def gtts_speak(audio_string):
     engine.say(audio_string)
     engine.runAndWait()
+'''
+
+'''
+#mimic3, sounds the best offline but is super slow
+def gtts_speak(audio_string):
+    os.system(f'mimic3 --voice en_US/cmu-arctic_low "{audio_string}" > audio/output.wav')
+
+    os.system("mpg123 audio/output.wav >/dev/null 2>&1")
+    os.remove("audio/output.wav")
 '''
